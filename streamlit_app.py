7bb3b436ced89a6e7d0e9dae11dd90fd5074d2e0
Kamil Breguła <mik-laj@users.noreply.github.com>
2024-11-04 16:43:05 +0000

    push code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
+import sqlglot
 import streamlit as st
+from streamlit_ace import st_ace
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+all_dialects = sorted(d.value for d in iter(sqlglot.Dialects) if d.value)
+
+left, right = st.columns(2)
+input_lng = left.selectbox("Input language",  all_dialects)
+output_lng = right.selectbox("Output language",  all_dialects)
+pretty = st.checkbox("Pretty format")
+input_sqls = st_ace(language="sql")
+
+if input_sqls:
+    try:
+        output_sqls = sqlglot.transpile(input_sqls, read=input_lng, write=output_lng, pretty=pretty)
+        for output_sql in output_sqls:
+            st.code(output_sql, language="sql")
+    except sqlglot.errors.ParseError as e:
+        st.write("**Transpilation errors:**")
+        st.table(e.errors)
